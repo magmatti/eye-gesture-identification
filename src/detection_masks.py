979352809c1ca7ff615af46c05f4d94661b33f74
Mateusz Wójtowicz <matiwojt@gmail.com>
2026-07-30 18:49:40 +0200

    delete detected_gesture stale column

--- src/detection_masks.py
+++ src/detection_masks.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 
 from .detection_config import DetectionConfig
-from .gesture_specs import BLINK, DETECTION_GESTURE_SPECS, FIXATION, SACCADE
+from .gesture_specs import BLINK, FIXATION, SACCADE
 
 
 # lables samples in the recording as one of 3 gestures or none using selected threshold values
@@ -25,10 +25,4 @@ def add_detection_masks(df: pd.DataFrame, cfg: DetectionConfig) -> pd.DataFrame:
     out[SACCADE.mask_column] = raw_saccade & ~raw_blink
     out[FIXATION.mask_column] = raw_fixation & ~raw_blink & ~raw_saccade
 
-    out["detected_gesture"] = np.select(
-        [out[spec.mask_column] for spec in DETECTION_GESTURE_SPECS],
-        [spec.name for spec in DETECTION_GESTURE_SPECS],
-        default="none",
-    )
-    
     return out
